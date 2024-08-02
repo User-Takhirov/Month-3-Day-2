3f38cdb2893b069050917755a761275486b57dcf
User-Takhirov <tohirovfayruz@gmail.com>
2024-08-02 17:51:13 +0500

    js-2

---
+++ main.js
@@ -0,0 +1,83 @@
+// 0 dan katta son bolsa true chiqarsin
+// let a = 24;
+
+// if (a > 0) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// toq sonlarni topish
+// let a=16;
+// if (a % 2 ==1) {
+//   console.log("toq");
+// } else {
+//   console.log("just");
+// }
+
+// jusft sonlarni topish
+// let a=16;
+// if (a % 2 ==0) {
+//   console.log("toq");
+// } else {
+//   console.log("just");
+// }
+
+// let a = 54;
+// let b = 43;
+// if (a > 2 && b <= 3) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// bir-biridan katta sonlarni tekshirsin
+// let a = 10;
+// let b = 10;
+// let c = 15;
+// if (a <= b && b <= c) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+//
+// let a = 18;
+// let b = 21;
+// let c = 35;
+// if (a >= b && b <= c || b <= a && b >= c) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+
+// just va toq sonlar 1 dan 100 gacha
+// let random = Math.round(Math.random() * 100);
+// if (random % 2 == 0) {
+//   console.log(random, "juft son");
+// } else {
+//   console.log(random, "toq son");
+// }
+
+
+
+
+// uy ishi
+
+// 1
+// let USD = 12600;
+// let EURO = 13600;
+// let avia = 500;
+// let hotel = 250;
+// let a = 120;
+// let dol = (avia + hotel) * USD;
+// let e = EURO * a;
+// let result = dol + e;
+// let balance = prompt("pulingizni kiriting");
+// console.log(balance);
+// if (balance >= dol || balance >= e) {
+//   console.log("Oq yo'l, Alisher!");
+// } else {
+//   console.log("Alisher, ozgina sabr qilish kerak bo’lar ekan");
+// }
